fec234df0e4629cd96c6e2f8d32b4d7fca708c8a
cxreiff <cooper@cxreiff.com>
2023-11-12 19:57:19 -0800

    clerk.dev setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -37,13 +37,14 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
+        <SignIn />
+        {/* <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
-        </div>
+        </div> */}
 
-        <CrudShowcase />
+        {/* <CrudShowcase /> */}
         <UserButton afterSignOutUrl="/" />
       </div>
     </main>

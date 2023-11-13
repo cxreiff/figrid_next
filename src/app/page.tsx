dc19880de8a769760f26b141956302ebdeb07e82
cxreiff <cooper@cxreiff.com>
2023-11-12 20:04:02 -0800

    made trpc routes public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello.query({ text: "from tRPC" });
+  const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -38,13 +38,12 @@ export default async function Home() {
           </Link>
         </div>
         <SignIn />
-        {/* <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
-        </div> */}
-
-        {/* <CrudShowcase /> */}
+        </div>
+        <CrudShowcase />
         <UserButton afterSignOutUrl="/" />
       </div>
     </main>
